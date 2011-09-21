cd34a22de4a3b02dbc5cf3417c4567b3f0ba362f
Stuart P. Bentley <stuart@testtrack4.com>
2011-09-21 09:07:40 -0400

    add duplicate name warning on list conversion

--- tools/convert_to_json.lua
+++ tools/convert_to_json.lua
@@ -1,7 +1,17 @@
 local people = {}
+local set = {}
+local in_filename = "list.txt"
+local out_filename = "people.json"
+local json = require "dkjson"
+
 local file = io.open(in_filename)
 for line in file:lines() do
   local name, desc = line:match"^(.-)%:%s*(.-)$"
+  if set[name] then
+    error(name)
+  else
+    set[name]=true
+  end
   people[#people+1] = {name = name, body = desc, links = {}}
 end
 file:close()
